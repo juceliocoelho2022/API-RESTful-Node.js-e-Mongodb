f27b50f52bacbef9834534d9d8bd8b7a68063bde
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-15 17:30:10 -0300

    MongoDB Install

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { urlencoded } = require("express");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 // forma de ler json / middlewares
@@ -15,10 +16,20 @@ app.use(express.json());
 
 //rota inicial / endpoint
 app.get("/", (req, res) => {
-
-//mostar req
-res.json({message: 'Oi Express!'})
-
+  //mostar req
+  res.json({ message: "Oi Express!" });
+  //mongodb+srv://jucelio:<password>@apicluster0.lza2dho.mongodb.net/?retryWrites=true&w=majority
 });
 //entregar uma porta
-app.listen(3000);
+
+const DB_USER ='jucelio'
+const DB_PASSWORD = encodeURIComponent('juce.1981')
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster0.lza2dho.mongodb.net/?retryWrites=true&w=majority `
+  )
+  .then(() => {
+    console.log("Conectamos ao MongoDB!");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
